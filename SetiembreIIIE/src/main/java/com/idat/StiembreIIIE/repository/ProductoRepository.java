a4f3bb312b26b2a82c40e94f183159494bf85f83
RzoPinedo <rzopinedo@gmail.com>
2022-09-13 22:16:33 -0500

    Semana 1

---
+++ SetiembreIIIE/src/main/java/com/idat/StiembreIIIE/repository/ProductoRepository.java
@@ -0,0 +1,16 @@
+package com.idat.StiembreIIIE.repository;
+
+import java.util.List;
+
+import com.idat.StiembreIIIE.model.Producto;
+
+public interface ProductoRepository {
+	
+	void guardar(Producto producto);
+	void actualizar(Producto producto);
+	void eliminar(Integer id);
+	List<Producto> listar();
+	Producto obtener(Integer id);
+	
+
+}
